8f88e762c34a661b94c2fe1bfb406ba9680e6194
Marie <marie.mouttet@hotmail.fr>
2019-01-05 19:45:49 +0100

    ScriptIso

---
+++ ZeldaLike_ProtoJanvier/ZeldaLike_ProtoJanvier/Assets/Scripts/DecorIsometrique.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorIsometrique : MonoBehaviour {
+
+    private float halfHeight;
+    private float halfWidth;
+    public float hauteurLigneBlanche;
+
+    private float posY;
+
+    private SpriteRenderer sRenderer;
+
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        halfHeight = sRenderer.bounds.size.y * 0.5f;
+        halfWidth = sRenderer.bounds.size.x * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        posY = (transform.position.y - halfHeight + hauteurLigneBlanche) * 50f;
+        sRenderer.sortingOrder = -Mathf.RoundToInt(posY);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 posLigneBlanche = new Vector3(transform.position.x, transform.position.y - halfHeight + hauteurLigneBlanche, transform.position.z);
+        Gizmos.DrawLine(posLigneBlanche + Vector3.left * halfWidth, posLigneBlanche + Vector3.right * halfWidth);
+    }
+
+}
