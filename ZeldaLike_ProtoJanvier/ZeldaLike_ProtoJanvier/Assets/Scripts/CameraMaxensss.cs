8bacf63dcda34aa2854a77a76d2556631a123426
Marie <marie.mouttet@hotmail.fr>
2019-01-05 23:22:40 +0100

    DebutCollider

---
+++ ZeldaLike_ProtoJanvier/ZeldaLike_ProtoJanvier/Assets/Scripts/CameraMaxensss.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMaxensss : MonoBehaviour {
+
+    public GameObject player;
+
+    [Header("Gauche - Droite")]
+    public float xMinimum;
+    public float xMaximum;
+
+    [Header("Bas - Haut")]
+    public float yMinimum;
+    public float yMaximum;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, xMinimum, xMaximum), Mathf.Clamp(player.transform.position.y, yMinimum, yMaximum), -10f);
+    }
+}
