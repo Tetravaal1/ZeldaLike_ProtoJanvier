6b03c10a0d84e2131f42704cdea363b1585394f1
Marie <marie.mouttet@hotmail.fr>
2019-01-06 16:21:01 +0100

    Fini

---
+++ ZeldaLike_ProtoJanvier/ZeldaLike_ProtoJanvier/Assets/Scripts/SonTruc.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonTruc : MonoBehaviour {
+
+    private AudioSource audioS;
+    public Transform Player;
+    public float distance;
+
+	void Start () {
+        audioS = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = (Player.position - transform.position).magnitude;
+        audioS.volume = 0.1f / (distance*2);
+	}
+}
---
+++ ZeldaLike_ProtoJanvier/ZeldaLike_ProtoJanvier/Assets/Scripts/Titros.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Titros : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        anim.SetBool("Inside", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        anim.SetBool("Inside", false);
+    }
+}
